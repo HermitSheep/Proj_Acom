9695ac28cf0e18700547515ab7af51668d66a364
HermitSheep <94017494+HermitSheep@users.noreply.github.com>
2022-05-19 19:35:21 +0100

    Update lab3.asm
    Teclados - 1 - varre várias linhas

--- lab3_22/pepe_16/lab3.asm
+++ lab3_22/pepe_16/lab3.asm
@@ -1,34 +1,41 @@
 ; *********************************************************************
 ; * IST-UL
 ; * Modulo:    lab3.asm
-; * Descrição: Exemplifica o acesso a um teclado.
-; *            Lê uma linha do teclado, verificando se há alguma tecla
+; * Descricao: Exemplifica o acesso a um teclado.
+; *            Le uma linha do teclado, verificando se ha alguma tecla
 ; *            premida nessa linha.
 ; *
-; * Nota: Observe a forma como se acede aos periféricos de 8 bits
-; *       através da instrução MOVB
+; * Nota: Observe a forma como se acede aos periferico de 8 bits
+; *       atraves da instrucao MOVB
 ; *********************************************************************
 
 ; **********************************************************************
 ; * Constantes
 ; **********************************************************************
-; ATENÇÃO: constantes hexadecimais que comecem por uma letra devem ter 0 antes.
-;          Isto não altera o valor de 16 bits e permite distinguir números de identificadores
-DISPLAYS   EQU 0A000H  ; endereço dos displays de 7 segmentos (periférico POUT-1)
-TEC_LIN    EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
-TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
-LINHA      EQU 8       ; linha a testar (4ª linha, 1000b)
+; ATENCAO: constantes hexadecimais que comecem por uma letra devem ter 0 antes.
+;          Isto nao altera o valor de 16 bits e permite distinguir numeros de identificadores
+DISPLAYS   EQU 0A000H  ; endereco dos displays de 7 segmentos (periferico POUT-1)
+TEC_LIN    EQU 0C000H  ; endereco das linhas do teclado (periferico POUT-2)
+TEC_COL    EQU 0E000H  ; endereco das colunas do teclado (periferico PIN)
+LINHA      EQU 8       ; linha a testar (4a linha, 1000b) em variavel para fazer os shifts
 MASCARA    EQU 0FH     ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 
 ; **********************************************************************
-; * Código
+; * Variaveis
+; **********************************************************************
+;LINHA WORD 8  ;  linha a testar (comeÃ§a pela quarta, mas deve andar por shifts atÃ© Ã  1 e voltar)
+              ;  (NÃ‚O TER DEFINIDA AO MESMO TEMPO QUE A CONSTANTE)
+
+
+; **********************************************************************
+; * Codigo
 ; **********************************************************************
 PLACE      0
 inicio:		
-; inicializações
-    MOV  R2, TEC_LIN   ; endereço do periférico das linhas
-    MOV  R3, TEC_COL   ; endereço do periférico das colunas
-    MOV  R4, DISPLAYS  ; endereço do periférico dos displays
+; inicializacoes
+    MOV  R2, TEC_LIN   ; endereco do periferico das linhas
+    MOV  R3, TEC_COL   ; endereco do periferico das colunas
+    MOV  R4, DISPLAYS  ; endereco do periferico dos displays
     MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 
 ; corpo principal do programa
@@ -36,24 +43,28 @@ ciclo:
     MOV  R1, 7
     MOVB [R4], R1      ; escreve linha e coluna a zero nos displays
 
-espera_tecla:          ; neste ciclo espera-se até uma tecla ser premida
+espera_tecla:          ; neste ciclo espera-se ate uma tecla ser premida
     MOV  R1, LINHA     ; testar a linha 4 
-    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
-    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
-    AND  R0, R5        ; elimina bits para além dos bits 0-3
-    CMP  R0, 0         ; há tecla premida?
-    JZ   espera_tecla  ; se nenhuma tecla premida, repete
+espera_linha:
+    CMP  R1, 0         ; jÃ¡ viu todas as linhas?
+    JZ   espera_tecla  ; se linha Ã© 0; repete
+    MOVB [R2], R1      ; escrever no periferico de saida (linhas)
+    MOVB R0, [R3]      ; ler do periferico de entrada (colunas)
+    SHR  R1, 1         ; faz shift left do ID da linha para a linha seguinte
+    AND  R0, R5        ; elimina bits para alem dos bits 0-3
+    CMP  R0, 0         ; ha tecla premida?
+    JZ   espera_linha  ; se nenhuma tecla premida, repete
                        ; vai mostrar a linha e a coluna da tecla
     SHL  R1, 4         ; coloca linha no nibble high
     OR   R1, R0        ; junta coluna (nibble low)
     MOVB [R4], R1      ; escreve linha e coluna nos displays
     
-ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
+ha_tecla:              ; neste ciclo espera-se ate NENHUMA tecla estar premida
     MOV  R1, LINHA     ; testar a linha 4  (R1 tinha sido alterado)
-    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
-    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
-    AND  R0, R5        ; elimina bits para além dos bits 0-3
-    CMP  R0, 0         ; há tecla premida?
-    JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera até não haver
+    MOVB [R2], R1      ; escrever no periferico de saida (linhas)
+    MOVB R0, [R3]      ; ler do periferico de entrada (colunas)
+    AND  R0, R5        ; elimina bits para alem dos bits 0-3
+    CMP  R0, 0         ; ha tecla premida?
+    JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera ate nao haver
     JMP  ciclo         ; repete ciclo
 
